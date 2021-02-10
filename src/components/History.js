60c41fa97c91275749534883775c6eb3ea174c0c
Chase Galatro <chasegalatro@gmail.com>
2021-02-10 13:56:02 -0500

    styled the app

--- src/components/History.js
+++ src/components/History.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 
 const History = ({ history, moveTo, currentMove }) => (
-  <ul>
-    {history.map((_, move) => (
-      <li key={move}>
-        <button
-          type="button"
-          onClick={() => moveTo(move)}
-          style={{ fontWeight: move === currentMove ? 'bold' : 'normal' }}
-        >
-          {move === 0 ? 'Go to game start' : `Go to move #${move}`}
-        </button>
-      </li>
-    ))}
-  </ul>
+  <div className="history-wrapper">
+    <ul className="history">
+      {history.map((_, move) => (
+        <li key={move}>
+          <button
+            type="button"
+            onClick={() => moveTo(move)}
+            className={`btn-move ${move === currentMove ? 'active' : ''}`}
+          >
+            {move === 0 ? 'Go to game start' : `Go to move #${move}`}
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default History;
